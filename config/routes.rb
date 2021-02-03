9399b6d58682ce6650fcc0df4529673b8f3400b4
Dayne Dewar <daynedewar@yahoo.com>
2021-02-03 11:56:54 -0500

    thanks eric

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # resources :courses
+  # resources :students
+  get "/students", to: "students#index", as: "students"
+  get "/students/new", to: "students#new", as: "new_student"
+  post "/students", to: "students#create"
+  get "/students/:id", to: "students#show", as: "student"
+  get "/students/:id/edit", to: "students#edit", as: "edit_student"
+  patch "/students/:id", to: "students#update"
+  delete "/students/:id", to: "students#destroy"
+
+  get "/courses", to: "courses#index", as: "courses"
+  get "/courses/new", to: "courses#new", as: "new_course"
+  post "/courses", to: "courses#create"
+  get "/courses/:id", to: "courses#show", as: "course"
+  get "/courses/:id/edit", to: "courses#edit", as: "edit_course"
+  patch "/courses/:id", to: "courses#update"
+  delete "/courses/:id", to: "courses#destroy"
+end
